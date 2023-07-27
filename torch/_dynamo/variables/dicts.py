2d7aafbf205f0859572eee5984695fa7f89a4f1c
angelayi <yiangela7@gmail.com>
2023-07-27 14:42:12 -0700

    refactor

--- torch/_dynamo/variables/dicts.py
+++ torch/_dynamo/variables/dicts.py
@@ -490,6 +490,6 @@ def _register_dynamo_dict_to_tree_spec():
         ConstDictVariable,
         _dictvariable_flatten,
         _dictvariable_unflatten,
-        pytree._dict_to_schema,
-        pytree._maybe_schema_to_dict,
+        pytree._dict_serialize,
+        pytree._dict_deserialize,
     )
