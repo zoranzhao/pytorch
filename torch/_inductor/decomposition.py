178bc29204381cc2d065f9886bdd4f9bd2dce9bc
chilli <chilli@meta.com>
2023-07-28 00:14:46 -0700

    Changes needed for transformer inference (includes Elias's cudagraph mutation for params)
    ghstack-source-id: cbddfaa6bb1596cb3f84e1c7d6c3349a2684f20f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/106192

--- torch/_inductor/decomposition.py
+++ torch/_inductor/decomposition.py
@@ -143,6 +143,23 @@ def convolution_backward(
     )
     return (grad_inp, grad_weight, grad_bias)
 
+@register_decomposition(aten.slice_scatter)
+def slice_scatter(self, src, dim=0, start=None, end=None, step=1):
+    if start == 0 and end >= 2**63 - 1 and step == 1:
+        return src
+    return NotImplemented
+
+@register_decomposition(aten.slice)
+def slice(self, dim=0, start=None, end=None, step=1):
+    if start == 0 and end >= 2**63 -1 and step == 1:
+        return self
+    return NotImplemented
+
+@register_decomposition(aten.mm)
+def mm(a, b):
+    if a.shape[0] == 1 or b.shape[1] == 1:
+        return (a.unsqueeze(2) * b.unsqueeze(0)).sum(dim=1)
+    return NotImplemented
 
 @register_decomposition([aten.log2])
 def log2(x):
