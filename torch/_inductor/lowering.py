e456f7fb32f4c6e57da6f06f6c14e291cc601941
leslie-fang-intel <leslie.fang@intel.com>
2023-08-03 13:14:39 +0800

    Enable the lowering of quantized maxpool2d
    ghstack-source-id: fde8b5f2b053f9e1bb76707d671bc50f2e7a725e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/105906

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -55,6 +55,7 @@ fallbacks = set()
 aten = torch.ops.aten
 tr_c10d = torch.ops.tr_c10d
 prims = torch.ops.prims
+quantized = torch.ops.quantized
 needs_realized_inputs = set()
 foreach_ops = set()
 
@@ -90,6 +91,7 @@ add_needs_realized_inputs(
         aten.upsample_nearest2d,
         aten.upsample_bicubic2d,
         aten._int_mm,
+        quantized.max_pool2d,
     ]
 )
 
@@ -3547,6 +3549,42 @@ def max_pool2d_with_indices_backward(
         dtype=grad_output.get_dtype(),
         inner_fn=fn,
         ranges=new_size,
+    )
+
+
+@register_lowering(quantized.max_pool2d, type_promotion_kind=None)
+def quantized_maxpool2d(
+    x, kernel_size, stride=None, padding=0, dilation=1, ceil_mode=False
+):
+    if padding == 0:
+        padding = [0, 0]
+    if dilation == 1:
+        dilation = [1, 1]
+    if not stride:
+        stride = kernel_size
+    kernel_size = pad_listlike(kernel_size, 2)
+    stride = pad_listlike(stride, 2)
+    padding = pad_listlike(padding, 2)
+    dilation = pad_listlike(dilation, 2)
+
+    x.realize()
+    *batch, h, w = x.get_size()
+
+    h_out, ceil_mode1 = pooling_size(h, 0, kernel_size, stride, padding, ceil_mode)
+    w_out, ceil_mode2 = pooling_size(w, 1, kernel_size, stride, padding, ceil_mode)
+
+    output_size = list(batch) + [h_out, w_out]
+
+    return TensorBox.create(
+        ir.QMaxpool2dPT2E.create(
+            x,
+            kernel_size,
+            stride,
+            padding,
+            dilation,
+            ceil_mode,
+            output_size,
+        )
     )
 
 
