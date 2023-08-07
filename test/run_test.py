b17416d678d3f027f5f0b7a8cc8871d6e951dec7
Catherine Lee <csl@fb.com>
2023-08-07 10:55:47 -0700

    no profiler tests

--- test/run_test.py
+++ test/run_test.py
@@ -1608,6 +1608,7 @@ def main():
 
     test_directory = str(REPO_ROOT / "test")
     selected_tests = get_selected_tests(options)
+    selected_tests = [x for x in selected_tests if "profile" not in x.name]
 
     if options.verbose:
         print_to_stderr(
